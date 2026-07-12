0cb8a5d91db34b0a9eea430cb1499308271163fc
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:03:24 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "monitor_private_link_scopes_id" {
+  description = "Map of id values across all monitor_private_link_scopes, keyed the same as var.monitor_private_link_scopes"
+  value       = { for k, v in azurerm_monitor_private_link_scope.monitor_private_link_scopes : k => v.id }
+}
 output "monitor_private_link_scopes_ingestion_access_mode" {
   description = "Map of ingestion_access_mode values across all monitor_private_link_scopes, keyed the same as var.monitor_private_link_scopes"
   value       = { for k, v in azurerm_monitor_private_link_scope.monitor_private_link_scopes : k => v.ingestion_access_mode }
