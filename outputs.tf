f725b33360fa8cffc4330a754e381f0130e726ac
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:49:17 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 output "monitor_private_link_scopes_id" {
   description = "Map of id values across all monitor_private_link_scopes, keyed the same as var.monitor_private_link_scopes"
-  value       = { for k, v in azurerm_monitor_private_link_scope.monitor_private_link_scopes : k => v.id }
+  value       = { for k, v in azurerm_monitor_private_link_scope.monitor_private_link_scopes : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "monitor_private_link_scopes_ingestion_access_mode" {
   description = "Map of ingestion_access_mode values across all monitor_private_link_scopes, keyed the same as var.monitor_private_link_scopes"
-  value       = { for k, v in azurerm_monitor_private_link_scope.monitor_private_link_scopes : k => v.ingestion_access_mode }
+  value       = { for k, v in azurerm_monitor_private_link_scope.monitor_private_link_scopes : k => v.ingestion_access_mode if v.ingestion_access_mode != null && length(v.ingestion_access_mode) > 0 }
 }
 output "monitor_private_link_scopes_name" {
   description = "Map of name values across all monitor_private_link_scopes, keyed the same as var.monitor_private_link_scopes"
-  value       = { for k, v in azurerm_monitor_private_link_scope.monitor_private_link_scopes : k => v.name }
+  value       = { for k, v in azurerm_monitor_private_link_scope.monitor_private_link_scopes : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "monitor_private_link_scopes_query_access_mode" {
   description = "Map of query_access_mode values across all monitor_private_link_scopes, keyed the same as var.monitor_private_link_scopes"
-  value       = { for k, v in azurerm_monitor_private_link_scope.monitor_private_link_scopes : k => v.query_access_mode }
+  value       = { for k, v in azurerm_monitor_private_link_scope.monitor_private_link_scopes : k => v.query_access_mode if v.query_access_mode != null && length(v.query_access_mode) > 0 }
 }
 output "monitor_private_link_scopes_resource_group_name" {
   description = "Map of resource_group_name values across all monitor_private_link_scopes, keyed the same as var.monitor_private_link_scopes"
-  value       = { for k, v in azurerm_monitor_private_link_scope.monitor_private_link_scopes : k => v.resource_group_name }
+  value       = { for k, v in azurerm_monitor_private_link_scope.monitor_private_link_scopes : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 output "monitor_private_link_scopes_tags" {
   description = "Map of tags values across all monitor_private_link_scopes, keyed the same as var.monitor_private_link_scopes"
-  value       = { for k, v in azurerm_monitor_private_link_scope.monitor_private_link_scopes : k => v.tags }
+  value       = { for k, v in azurerm_monitor_private_link_scope.monitor_private_link_scopes : k => v.tags if v.tags != null && length(v.tags) > 0 }
 }
 
